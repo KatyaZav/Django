c49d888434a835ca321eaea0bba39975530c31f9
KatyaZav <party2015ru@mail.ru>
2022-05-01 18:56:40 +0500

    3.2 Worked!!!!!

--- WebDjango/Backend/urls.py
+++ WebDjango/Backend/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    #path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
 ]
--- WebDjango/Backend/views.py
+++ WebDjango/Backend/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 
 from django.http import HttpResponse
 
-@login_required
+#@login_required
 def register(request): 
     if request.method == 'POST': 
         form = UserForm(request.POST) 
@@ -21,8 +21,8 @@ def register(request):
     form = UserForm() 
     return render(request, 'register.html', {'form': form})
 
-"""def index(request):
-    return HttpResponse('index page')"""
+def index(request):
+    return HttpResponse('index page')
 
 def user_login(request): 
     form = UserForm()
